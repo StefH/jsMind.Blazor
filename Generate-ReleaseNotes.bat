a2b6d4a671bc390b82743519a810c78a83c1aaa6
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-06-30 17:18:16 +0200

    .

--- Generate-ReleaseNotes.bat
+++ Generate-ReleaseNotes.bat
@@ -3,5 +3,3 @@ rem https://github.com/StefH/GitHubReleaseNotes
 SET version=0.0.15
 
 GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid doc --version %version%
-
-GitHubReleaseNotes --output PackageReadme.md --skip-empty-releases --exclude-labels question invalid doc --template PackageReleaseNotes.template --version %version%

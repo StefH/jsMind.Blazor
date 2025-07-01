c803e188b29f93259fa0b84f6418cd5b0e2821f6
StefH <StefH@users.noreply.github.com>
2025-07-01 05:20:51 +0000

    Deploy to GitHub pages

---
+++ _content/jsMind.Blazor/jsmind-interop.js
@@ -0,0 +1,239 @@
+var MindMap = MindMap || {};
+
+const instances = {};
+
+MindMap.setDocumentTitle = function (title) {
+    document.title = title;
+};
+
+MindMap.show = function (dotnetReference, containerId, mindMapOptions, mindMapData) {
+    const mind = {
+        "meta": {
+            "name": containerId,
+            "author": "Stef Heyenrath",
+            "version": "1.0"
+        },
+        "format": mindMapData.format,
+        "data": mindMapData.data
+    };
+
+    const options = {
+        container: containerId,
+        editable: mindMapOptions.editable,
+        theme: mindMapOptions.theme
+    }
+
+    const mm = window.jsMind.show(options, mind);
+
+    // ReadOnly
+    setReadOnly(mm, mindMapOptions.readOnly);
+
+    // MultiSelect
+    mm["multiSelect"] = mindMapOptions.multiSelect;
+
+    const eventHandler = async function (type, data) {
+        // show: 1, resize: 2, edit: 3, select: 4
+        try {
+            switch (type) {
+                case 1:
+                    //dotnetReference.invokeMethodAsync("OnShowCallback", data);
+                    break;
+
+                case 2:
+                    await dotnetReference.invokeMethodAsync("OnResizeCallback", { evt: "resize", containerId: containerId });
+                    break;
+
+                case 3:
+                    await dotnetReference.invokeMethodAsync("OnEditCallback", data);
+                    break;
+
+                case 4:
+                    await dotnetReference.invokeMethodAsync("OnSelectCallback", data);
+                    break;
+            }
+        }
+        catch (e) {
+            console.warn(e);
+        }
+    }
+
+    // Custom
+    if (mindMapOptions.multiSelect) {
+        MindMap.clearSelect(containerId);
+
+        const mousedownHandleMultiSelect = function (e) {
+            if (!mm.options.default_event_handle.enable_mousedown_handle) {
+                return;
+            }
+
+            e.preventDefault();
+
+            const element = e.target || event.srcElement;
+            const id = this.view.get_binded_nodeid(element);
+            if (id && element.tagName.toLowerCase() === "jmnode") {
+                const node = mm.get_node(id);
+
+                let selectedNodeId;
+
+                // Check if already selected
+                const index = mm.selectedNodes.indexOf(id);
+                if (index > -1) {
+                    // Remove from list
+                    mm.selectedNodes.splice(index, 1);
+
+                    // Remove the 'selected' class
+                    updateSelectedClass(node, false);
+
+                    // Set selectedId to null
+                    selectedNodeId = null;
+                } else {
+                    // Add to list
+                    mm.selectedNodes.push(id);
+
+                    // Set selectedId to this node
+                    selectedNodeId = id;
+                }
+
+                mm.selectedNodes.forEach(selectedId => {
+                    const selectedNode = mm.get_node(selectedId);
+                    updateSelectedClass(selectedNode, true);
+                });
+
+                dotnetReference.invokeMethodAsync("OnMultiSelectCallback", { id: selectedNodeId, ids: mm.selectedNodes });
+            }
+        }
+
+        mm.view.add_event(mm, "mousedown", mousedownHandleMultiSelect);
+    }
+
+    mm.add_event_listener(eventHandler);
+
+    // Keep a reference to the javascript MindMap object
+    instances[containerId] = mm;
+
+    // Call a callback to indicate that the MindMap is completely shown (and the instances correctly updated with the containerId)
+    dotnetReference.invokeMethodAsync("OnShowCallback", { evt: "done", containerId: containerId });
+}
+
+MindMap.destroy = function (containerId) {
+    instances[containerId] = null;
+    delete instances[containerId];
+}
+
+MindMap.addNode = function (containerId, parentId, id, topic, data) {
+    instances[containerId] && instances[containerId].add_node(parentId, id, topic, data);
+}
+
+MindMap.updateNode = function (containerId, id, topic) {
+    instances[containerId] && instances[containerId].update_node(id, topic);
+}
+
+MindMap.removeNode = function (containerId, id) {
+    instances[containerId] && instances[containerId].remove_node(id);
+}
+
+MindMap.expandNode = function (containerId, id) {
+    instances[containerId] && instances[containerId].expand_node(id);
+}
+
+MindMap.collapseNode = function (containerId, id) {
+    instances[containerId] && instances[containerId].collapse_node(id);
+}
+
+MindMap.expand = function (containerId) {
+    instances[containerId] && instances[containerId].expand_all();
+}
+
+MindMap.expandToDepth = function (containerId, depth) {
+    instances[containerId] && instances[containerId].expand_to_depth(depth);
+}
+
+MindMap.collapse = function (containerId) {
+    instances[containerId] && instances[containerId].collapse_all();
+}
+
+MindMap.selectNode = function (containerId, id) {
+    instances[containerId] && instances[containerId].select_node(id);
+}
+
+MindMap.selectNodes = function (containerId, nodes) {
+    if (instances[containerId] && instances[containerId].multiSelect) {
+        instances[containerId].selectedNodes = [];
+
+        nodes.forEach(node => {
+            const foundNode = instances[containerId].get_node(node.id);
+            updateSelectedClass(foundNode, true);
+
+            instances[containerId].selectedNodes.push(node.id);
+        });
+    }
+}
+
+MindMap.getNode = function (containerId, id) {
+    return instances[containerId] ? mapNode(instances[containerId].get_node(id)) : undefined;
+}
+
+MindMap.clearSelect = function (containerId) {
+    const mm = instances[containerId];
+    if (mm) {
+        if (mm.multiSelect) {
+            if (mm.selectedNodes) {
+                mm.selectedNodes.forEach(node => {
+                    updateSelectedClass(mm.get_node(node), false);
+                });
+            }
+            mm.selectedNodes = [];
+        } else {
+            mm.select_clear();
+        }
+    }
+}
+
+MindMap.setTheme = function (containerId, theme) {
+    instances[containerId] && instances[containerId].set_theme(theme);
+}
+
+MindMap.disableEdit = function (containerId) {
+    instances[containerId] && instances[containerId].disable_edit();
+}
+
+MindMap.enableEdit = function (containerId) {
+    instances[containerId] && instances[containerId].enable_edit();
+}
+
+MindMap.isEditable = function (containerId) {
+    return instances[containerId] ? instances[containerId].get_editable() : false;
+}
+
+MindMap.setReadOnly = function (containerId, isReadOnly) {
+    return instances[containerId] ? setReadOnly(instances[containerId], isReadOnly) : false;
+}
+
+updateSelectedClass = function (node, set) {
+    if (set && !(/\s*selected\b/i).test(node._data.view.element.className)) {
+        node._data.view.element.className += " selected";
+    }
+    else if (!set) {
+        node._data.view.element.className = node._data.view.element.className.replace(/\s*selected\b/ig, "");
+    }
+}
+
+mapNode = function (node) {
+    // Skip children property (TypeError: Converting circular structure to JSON)
+    return {
+        id: node.id,
+        topic: node.topic,
+        expanded: node.expanded,
+        direction: node.direction,
+        data: node.data,
+        parentId: node.parentId
+    };
+}
+
+setReadOnly = function (mm, isReadOnly) {
+    mm.options.default_event_handle = {
+        enable_mousedown_handle: !isReadOnly,
+        enable_click_handle: !isReadOnly,
+        enable_dblclick_handle: !isReadOnly
+    };
+}
---
+++ service-worker-assets.js
@@ -0,0 +1,273 @@
+self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-47DEQpj8HBSa+\/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=",
+      "url": ".nojekyll"
+    },
+    {
+      "hash": "sha256-Akm5I5BeUyTpCvrLan5IXc\/54DT9tS21aizwBmH+VjE=",
+      "url": "404.html"
+    },
+    {
+      "hash": "sha256-t9SxX6hu0vBjC2a9Y1FOcRKvaV822VKDORjvXK+fWHU=",
+      "url": "blazor.webassembly.patched.formatted.js"
+    },
+    {
+      "hash": "sha256-X5ZaNwnWtG56oyGo52S9m5xqfehjq36Nxz8eYKcuNPc=",
+      "url": "blazor.webassembly.patched.js"
+    },
+    {
+      "hash": "sha256-CLMwCdXSHqGZJPkmVzrW5AzX0GqL2ZMFG0ZMmKJhCxs=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-YLGeXaapI0\/5IgZopewRJcFXomhRMlYYjugPLSyNjTY=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-xMZ0SaSBYZSHVjFdZTAT\/IjRExRIxSriWcJLcA9nkj0=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-+Q44zfEaCMmXduni5Td+IgCbk8sSUQwES2nWs+KKQz0=",
+      "url": "css\/open-iconic\/FONT-LICENSE"
+    },
+    {
+      "hash": "sha256-BJ\/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=",
+      "url": "css\/open-iconic\/font\/css\/open-iconic-bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-OK3poGPgzKI2NzNgP07XMbJa3Dz6USoUh\/chSkSvQpc=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.eot"
+    },
+    {
+      "hash": "sha256-sDUtuZAEzWZyv\/U1xl\/9D3ehyU69JE+FvAcu5HQ+\/a0=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.otf"
+    },
+    {
+      "hash": "sha256-oUpLdS+SoLJFwf4bzA3iKD7TCm66oLkTpAQlVJ2s1wc=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.svg"
+    },
+    {
+      "hash": "sha256-p+RP8CV3vRK1YbIkNzq3rPo1jyETPnR07ULb+HVYL8w=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.ttf"
+    },
+    {
+      "hash": "sha256-cZPqVlRJfSNW0KaQ4+UPOXZ\/v\/QzXlejRDwUNdZIofI=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.woff"
+    },
+    {
+      "hash": "sha256-s\/Is6Ey6jfNAEfXUIOyHrXXX+RcA8hzchYnuOIWUMl4=",
+      "url": "css\/open-iconic\/ICON-LICENSE"
+    },
+    {
+      "hash": "sha256-9wdNXQFE78LCNHo+Hq2eXMTx+YBf2gjsufVTJc8dAV0=",
+      "url": "css\/open-iconic\/README.md"
+    },
+    {
+      "hash": "sha256-M8FQ1Iwes5W4pr2FqCgsWAi5Yd3mnRf73TJTKtCvPrw=",
+      "url": "decode.js"
+    },
+    {
+      "hash": "sha256-\/zb9tna3jkluvZFWy9F3lz+MAT9zD7Hn53EOBrRq6qU=",
+      "url": "favicon.ico"
+    },
+    {
+      "hash": "sha256-97KsLYzQCsagVVsD9EQJNt2lvq5YahcNvBTjgWG37Jo=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-nPm3Wnhqm2zvZPHjoh0jUZz4Cst4YHRJnnrCKOwGhI4=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-wC+zAyYHVTNzevCw3SFvHI4jG51pV1+b5sQ3Rj11QGI=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-pu7MO47B6So4jcW6iY2UI11lBZYz2EQnJKiYNsOpNJg=",
+      "url": "_framework\/dotnet.6.0.36.gwq1pilaic.js"
+    },
+    {
+      "hash": "sha256-vrFYQt55a4tGoDGoyRfjjGa1nSecx646RK78iTzPWCM=",
+      "url": "_framework\/dotnet.timezones.blat"
+    },
+    {
+      "hash": "sha256-tISvy4xFegogfGkIyyz9o\/8bI+dPsrbp0\/DIdANcHBI=",
+      "url": "_framework\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-WPyI4hWDPnOw62Nr27FkzGjdbucZnQD+Ph+GOPhAedw=",
+      "url": "_framework\/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-4RwaPx87Z4dvn77ie\/ro3\/QzyS+\/gGmO3Y\/0CSAXw4k=",
+      "url": "_framework\/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-OxylFgLJlFqixsj+nLxYVsv5iZLvfIKMpLf9hrWaChA=",
+      "url": "_framework\/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-bNukpgjXH7MrBe9B4UYnURpnPYychwHmSgLMM7WAScA=",
+      "url": "_framework\/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-+x2bBAJ\/9dnI42v9UA2Z8HD8IHufhbMqhAmZ77Fk+TQ=",
+      "url": "_framework\/jsMind.Blazor.dll"
+    },
+    {
+      "hash": "sha256-Lx8nG5DgvSyzGKR+RF\/c29ap9iYDRB8kDZeWxBf3+Mk=",
+      "url": "_framework\/jsMind.WASM.App.dll"
+    },
+    {
+      "hash": "sha256-CeUHidya7soC63CIiC8CUVdjnK9bNYOEp\/63wcsRQ\/w=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-3LRkHoGuTxAt4un\/eNjlxSTJHBi\/RweRM1hL0Saa8C4=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-d4EfeXiZsQmdl6aZ6NEULU+Xrp2E9fDZ10Agr4WMos4=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-Yd5HWoJ8z3pGHLdLRk56mDLrXa7AkXv9+C\/CdwnvOCk=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-qBPk1O\/mfd\/P9kDywnOU5wZ7vka6iecseJsfIp04HMY=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-qCkDd0PaEIJeCuBjIZeHopcREnNAfhCkOxkTfDvRJCU=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-mzm1DAyyFWnecg\/KaGGQiR3B5\/UTem38CQQX3Dk+1Z8=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-DG4t8WcZydm0E7u9Jba98S15iYEuwN\/7Lc4ZxhOzolk=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-CVSCilm+KDv18ORRnt\/rXZYKhqmHWldPYRwGeRNbLfg=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-rGXa3leYNXH\/Cu3cszLotzdH8tyAVGZWAnSc7iI3kV4=",
+      "url": "_framework\/Microsoft.Extensions.Logging.dll"
+    },
+    {
+      "hash": "sha256-rKuFMNxPlPpYGIA4ZkgZ2osef\/klzJh\/awzaKp51bto=",
+      "url": "_framework\/Microsoft.Extensions.Options.dll"
+    },
+    {
+      "hash": "sha256-QRewYnpJ6vl8gGMXBS2gN++\/sQ9hKcuv+rhy586Bbxs=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-Mo9nrYNrDUzgj14erb2tFqcFZARXanLt8QVlv3fkQN8=",
+      "url": "_framework\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-Fu9b3h5X0e7LvSjXzf7gMel0T48fy1JOlLde3zs4KyU=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-NLhRvB67XyzyRkH6AmyXh6vzfxnfmJ9\/b9Oqe5CMa0o=",
+      "url": "_framework\/netstandard.dll"
+    },
+    {
+      "hash": "sha256-Du72SoWeQ48x354gmZpdSFJd+N+0aJdaYD2n87ELK5Q=",
+      "url": "_framework\/System.Collections.Concurrent.dll"
+    },
+    {
+      "hash": "sha256-d7WlSWaFLcMzJg7lYU2ZXcjbpbrRGz\/ATzG0fw\/mWBQ=",
+      "url": "_framework\/System.Collections.dll"
+    },
+    {
+      "hash": "sha256-YzYpr0u1TBvUblFT2c0EMUTvgInrsws9AArNlb1TXQI=",
+      "url": "_framework\/System.ComponentModel.dll"
+    },
+    {
+      "hash": "sha256-rVqml17cDvewaOSBnN3mroN6PpAeXcJgh0UHVM7two0=",
+      "url": "_framework\/System.Console.dll"
+    },
+    {
+      "hash": "sha256-p+teWehX3PBkbzbU5P0VUGWv3d929wxoFwXIict\/o3I=",
+      "url": "_framework\/System.Linq.dll"
+    },
+    {
+      "hash": "sha256-avh7xgpn9PZ070\/dIMNwMjOEROOY8X68XCQqmxZe+iY=",
+      "url": "_framework\/System.Memory.dll"
+    },
+    {
+      "hash": "sha256-ed28Jo7hfzRhkw2pCzP3AcB9BSG0lb9NN1doew2v0yk=",
+      "url": "_framework\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-069Odo7vnDyNyKyrbxUlVsdm12XYTEJ77lu67tGnAuw=",
+      "url": "_framework\/System.Net.Primitives.dll"
+    },
+    {
+      "hash": "sha256-kBCi2UI6kDIg4xQNb\/8xSLclDr7LY441RdoMibY8vnk=",
+      "url": "_framework\/System.Private.CoreLib.dll"
+    },
+    {
+      "hash": "sha256-99apOUrc2icXy3dhvIU5Ucj20tbbVzmlkAxI1Z+WrNM=",
+      "url": "_framework\/System.Private.Runtime.InteropServices.JavaScript.dll"
+    },
+    {
+      "hash": "sha256-kJU6WNC\/2gK199SAIru2K+IXUDq+uPXc6w45PNndpc8=",
+      "url": "_framework\/System.Private.Uri.dll"
+    },
+    {
+      "hash": "sha256-GgRIr7NS93sUSAQCEL2kVPqYnoznlgMjiJHDHT+iukE=",
+      "url": "_framework\/System.Runtime.CompilerServices.Unsafe.dll"
+    },
+    {
+      "hash": "sha256-jdd8nHMtM9DmUXgdSc6qOrsWt9YKT7SoVICau40S7kU=",
+      "url": "_framework\/System.Runtime.dll"
+    },
+    {
+      "hash": "sha256-lWdclCloD07MI+1xM03IY6mhQxEOZyYKjyETY9Qxrrs=",
+      "url": "_framework\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-RNq0YU+QUtCgzQlNxvD97OfZDgc7e4zSCqeJxOIkRZM=",
+      "url": "_framework\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-BA6X+19sE6oO5WoRH+SoYZ6D87rbcwqDPiQDLOxEIJQ=",
+      "url": "_content\/jsMind.Blazor\/jsmind-interop.js"
+    },
+    {
+      "hash": "sha256-q975lkNFGfMFNm5wwh5\/CDaAWydNz2wua+gXmf0wAbw=",
+      "url": "_content\/jsMind.Blazor\/jsmind-interop.min.js"
+    },
+    {
+      "hash": "sha256-c4Yg71ITmpfR3jkn+5QCREkflvxhTBE2zHgPaivu9zo=",
+      "url": "_content\/jsMind.Blazor\/jsmind.css"
+    },
+    {
+      "hash": "sha256-zDKuA9KyNwLi86M7k5ibcOrmSwO4h8rY2DTx5XtFAho=",
+      "url": "_content\/jsMind.Blazor\/jsmind.js"
+    },
+    {
+      "hash": "sha256-EVy7+ngWO\/hZK8InSGGvbhCM9J2uJSsDZ\/bo7meukYc=",
+      "url": "_content\/jsMind.Blazor\/jsmind.min.css"
+    },
+    {
+      "hash": "sha256-csI8xXUQWB\/Cp3c5BHsU8byLdz92YrhmCMST1+5r0OA=",
+      "url": "_content\/jsMind.Blazor\/jsmind.min.js"
+    },
+    {
+      "hash": "sha256-8ZRc1sGeVrPBx4lD717BgRaQekyh78QKV9SKsdt638U=",
+      "url": "_content\/jsMind.Blazor\/keep_this_folder.txt"
+    }
+  ],
+  "version": "DxBF7oOp"
+};
